916ec98009d58ca68107fb4671411d8d292a96a4
Donald Hutchison <git@toastymofo.net>
2014-10-17 14:21:31 +0200

    Site updated at: 2014-10-17 12:21:31 UTC

--- assets/js/subtitle.js
+++ assets/js/subtitle.js
@@ -4,25 +4,25 @@ var getSubtitle = function () {
         "the man who shot liberty valance",
         "qrrbrbirlbel!!!!!!",
         "(⌐■_■)--︻╦╤─ - - -",
-        "A miserable little pile of secrets!",
-        "Come on and slam, and welcome to the jam",
-        "tunnel snakes rule",
+        "A miserable little pile of secrets!", // castlevania sotn
+        "Come on and slam, and welcome to the jam",//space jam
+        "tunnel snakes rule",//fallout
         "r.kachowski",
         "software developer and all round swell guy",
         "whoa, what - really??! wait, what?!",
-        "I heard you're idea's and their definitely good",
-        "don't you mean, 'snake, in the jewel closet'??",
+        "I heard you're idea's and their definitely good",//xkcd
+        "don't you mean, 'snake, in the jewel closet'??",//the least surprised
         '[ $[ $RANDOM % 6 ] == 0 ] && rm -fr / || echo "click"',
-        "The fact has not created in me. A sense of obligation.",
-        "I'm the biggest cowboy in these parts, if there were bigger I killed 'em all",
-        "Remember what I yelled, and remind me what I said",
-        "YOU HAVE TO THINK LIKE A STAR!!",
-        "I'm into the finer things, like, red wine and time machines",
-        "I'm into the finer things, like, red wine and time machines",
-        "How can I describe it to you? The sly eagle hides its claws.",
+        "The fact has not created in me. A sense of obligation.",//a man said to the universe
+        "I'm the biggest cowboy in these parts, if there were bigger I killed 'em all",//cowboyana
+        "Remember what I yelled, and remind me what I said",//cowboyana
+        "YOU HAVE TO THINK LIKE A STAR!!",//mondo medicals
+        "I'm into the finer things, like, red wine and time machines",//a_rival
+        "I'm into the finer things, like, red wine and time machines",//a_rival
+        "How can I describe it to you? The sly eagle hides its claws.",//ff9
         "awesome! nice! pretty sweet!",
-        "Here I go, deep type flow, Jacque Cousteau could never get this low",
-        "a fledgling freelance professor, one who will die young and make no lasting impression."];
+        "Here I go, deep type flow, Jacque Cousteau could never get this low",//wu tang
+        "a fledgling freelance professor, one who will die young and make no lasting impression."];//_why
 
     return texts[Math.floor(Math.random()*texts.length)];
 };
